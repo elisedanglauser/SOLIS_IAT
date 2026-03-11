14a6d4722e92bf6c40a7af600f125c7c53c41969
elisedanglauser <50578238+elisedanglauser@users.noreply.github.com>
2026-03-11 15:29:45 +0100

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -244,7 +244,7 @@ function makePersistentTrial(stimulus,key,block,type){
 
 return{
 type:jsPsychHtmlKeyboardResponse,
-stimulus:`<img src="${stimulus}" sstyle="max-height:100vh;width:auto;">`,
+stimulus:`<img src="${stimulus}">`,
 choices:"NO_KEYS",
 trial_duration:null,
 
@@ -358,6 +358,7 @@ fullscreen_mode:false
 
 
 jsPsych.run(timeline);
+
 
 
 
